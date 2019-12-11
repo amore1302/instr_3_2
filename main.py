85fff0cbc108b7ceffea4494bed3325e0bf7237c
amore1302 <amore1302@mail.ru>
2019-12-11 18:41:36 +0300

    учтены замечания по проекту
    учтены замечания по проекту

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ import os
 
 
 def is_user_exist(current_user):
-    if bot.get_user_id_from_username(current_user) != None:
-        return True
-    return False
+    return (bot.get_user_id_from_username(current_user) != None )
 
 
 def get_all_users_from_one_coment(current_comment):
+	# как искать регулярное выражение для инстаграмм описано в ссылке
+	#     https://blog.jstassen.com/2016/03/code-regex-for-instagram-username-and-hashtags/
     reg_expr_for_user_instagram = "(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)"
     resultat_list = []
     users_current_coment = re.findall(reg_expr_for_user_instagram, current_comment)
@@ -25,16 +25,18 @@ def get_all_users_from_one_coment(current_comment):
 
 
 def main():
+    load_dotenv()
+    bot = Bot()
+
     parser = argparse.ArgumentParser()
     parser.add_argument("echo")
     args = parser.parse_args()
     url_post_istagram = args.echo
-    print(url_post_istagram)
 
 
-    inst_login = os.getenv("INTGR_LOGIN")
-    inst_passwd = os.getenv("INTGR_PASSWD")
-    bot.login(username=inst_login, password=inst_passwd)
+    instagram_login = os.getenv("INTGR_LOGIN")
+    instagram_passwd = os.getenv("INTGR_PASSWD")
+    bot.login(username=instagram_login, password=instagram_passwd)
 
     media_id = bot.get_media_id_from_link(url_post_istagram)
 
@@ -48,7 +50,7 @@ def main():
         print("Список подписчиков Пустой  =  Все остановили ")
         return
 
-    ind1 = 0
+    count_user_and_usercomment = 0
     all_coments = bot.get_media_comments_all(media_id, True)
     set_who_commented = set()
     for current_coment in all_coments:
@@ -57,8 +59,8 @@ def main():
             continue
         current_user = current_user_and_usercomment[0]
         current_user_str = str(current_user)
-        ind1 = ind1 + 1
-        print("{1} {0}".format(current_user_str, ind1))
+        count_user_and_usercomment = count_user_and_usercomment + 1
+        print("{1} {0}".format(current_user_str, count_user_and_usercomment))
         id_curent_user_int = bot.get_user_id_from_username(current_user)
         if id_curent_user_int == None:
             continue
@@ -77,8 +79,4 @@ def main():
 
 
 if __name__ == '__main__':
-    load_dotenv()
-    inst_login = os.getenv("INTGR_LOGIN")
-    inst_passwd = os.getenv("INTGR_PASSWD")
-    bot = Bot()
     main()
