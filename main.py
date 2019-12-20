a3423a714b7313beeb6d5d173776e8533f88bff6
amore1302 <amore1302@mail.ru>
2019-12-20 21:26:38 +0300

    улучшение текста
    уу

--- main.py
+++ main.py
@@ -46,19 +46,20 @@ def main():
         print("Список подписчиков Пустой  =  Все остановили ")
         return
 
-    who_invited_a_friend_count = 0
+    comment_users = {}
     all_comments = bot.get_media_comments_all(media_id, True)
     prize_candidates = set()
     for current_comment in all_comments:
         current_user_and_usercomment = get_all_users_from_one_comment(current_comment)
         if len(current_user_and_usercomment) < 2:
             continue
-        current_user = current_user_and_usercomment[0]
-        who_invited_a_friend_count = who_invited_a_friend_count + 1
-        print("{1} {0}".format(current_user_str, who_invited_a_friend_count))
+        for current_user in current_user_and_usercomment[1:] :
+            if is_user_exist(current_user) :
+                comment_users.add(current_user)
+
+    for current_user in comment_users:
+        print("{0}".format(current_user))
         id_current_user_int = bot.get_user_id_from_username(current_user)
-        if id_current_user_int is None:
-            continue
         id_curtent_user_str = str(id_current_user_int)
 
         if id_curtent_user_str in followers_users and  id_curtent_user_str in like_users :
