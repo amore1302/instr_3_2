e698342ce5b7e78f1231b780cc29ab3c80b0a53b
amore1302 <amore1302@mail.ru>
2019-12-30 20:40:51 +0300

    улучшена логика программы
    улучшена логика программы

---
+++ main — копия.py
@@ -0,0 +1,78 @@
+﻿from instabot import Bot
+from dotenv import load_dotenv
+from pprint import pprint
+import argparse
+import re
+import os
+
+
+
+def is_user_exist(current_user):
+    return bot.get_user_id_from_username(current_user) is not None 
+
+
+def get_all_users_from_one_comment(current_comment):
+	# как искать регулярное выражение для инстаграмм описано в ссылке
+	#     https://blog.jstassen.com/2016/03/code-regex-for-instagram-username-and-hashtags/
+    reg_expr_for_user_instagram = "(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)"
+
+    mentions = re.findall(reg_expr_for_user_instagram, current_comment)
+    filtered_mentions = {mention for mention in mentions if is_user_exist(mention)}
+    return filtered_mentions
+	
+
+def main():
+    load_dotenv()
+    bot = Bot()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("name")
+    parser.add_argument("echo")
+    args = parser.parse_args()
+    url_name_user = args.name
+    url_post_istagram = args.echo
+
+    instagram_login = os.getenv("INSTAGRAM_LOGIN")
+    instagram_passwd = os.getenv("INSTAGRAM_PASSWD")
+    bot.login(username=instagram_login, password=instagram_passwd)
+
+    media_id = bot.get_media_id_from_link(url_post_istagram)
+
+    liked_users = bot.get_media_likers(media_id)
+
+    start_post_user_id = bot.get_user_id_from_username(url_name_user) 
+
+    followers_users = bot.get_user_followers(start_post_user_id)
+
+
+    if not followers_users :
+        print("Список подписчиков Пустой  =  Все остановили ")
+        return
+
+    comment_users = {}
+    all_comments = bot.get_media_comments_all(media_id, True)
+    prize_candidates = set()
+    for current_comment in all_comments:
+        current_user_and_usercomment = get_all_users_from_one_comment(current_comment)
+        for current_user in current_user_and_usercomment[1:] :
+            if is_user_exist(current_user) :
+                comment_users.add(current_user)
+
+    for current_user in comment_users:
+        print("{0}".format(current_user))
+        id_current_user_int = bot.get_user_id_from_username(current_user)
+        id_curtent_user_str = str(id_current_user_int)
+
+        if id_curtent_user_str in followers_users and  id_curtent_user_str in liked_users :
+            prize_candidates.add(current_user)
+
+    if not prize_candidates :
+        print("Список пользователей по всем условиям Пустой !!!")
+        return
+
+    print("Список Пользователей кто выполнил все условия конкурса Instagram")
+    pprint(prize_candidates)
+
+
+if __name__ == '__main__':
+    main()
--- main.py
+++ main.py
@@ -8,23 +8,23 @@ import os
 
 
 def is_user_exist(current_user):
-    return bot.get_user_id_from_username(current_user) is not None 
+    if bot.get_user_id_from_username(current_user) != None:
+        return True
+    return False
 
 
-def get_all_users_from_one_comment(current_comment):
-	# как искать регулярное выражение для инстаграмм описано в ссылке
-	#     https://blog.jstassen.com/2016/03/code-regex-for-instagram-username-and-hashtags/
+def get_all_users_from_one_coment(current_comment):
     reg_expr_for_user_instagram = "(?:@)([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)"
+    resultat_list = []
+    users_current_coment = re.findall(reg_expr_for_user_instagram, current_comment)
+    for current_user in users_current_coment:
+        if is_user_exist(current_user):
+            if not (current_user in resultat_list):
+                resultat_list.append(current_user)
+    return resultat_list
 
-    mentions = re.findall(reg_expr_for_user_instagram, current_comment)
-    filtered_mentions = {mention for mention in mentions if is_user_exist(mention)}
-    return filtered_mentions
-	
 
 def main():
-    load_dotenv()
-    bot = Bot()
-
     parser = argparse.ArgumentParser()
     parser.add_argument("name")
     parser.add_argument("echo")
@@ -32,47 +32,51 @@ def main():
     url_name_user = args.name
     url_post_istagram = args.echo
 
-    instagram_login = os.getenv("INSTAGRAM_LOGIN")
-    instagram_passwd = os.getenv("INSTAGRAM_PASSWD")
-    bot.login(username=instagram_login, password=instagram_passwd)
+    inst_login = os.getenv("INTGR_LOGIN")
+    inst_passwd = os.getenv("INTGR_PASSWD")
+    bot.login(username=inst_login, password=inst_passwd)
 
     media_id = bot.get_media_id_from_link(url_post_istagram)
+    users_like = bot.get_media_likers(media_id)
 
-    liked_users = bot.get_media_likers(media_id)
+    id_user_start_post = bot.get_user_id_from_username(url_name_user)
+    followers_users = bot.get_user_followers(id_user_start_post)
 
-    start_post_user_id = bot.get_user_id_from_username(url_name_user) 
+	if liked_users:
+		print("Не нашли список лайков поста")
+		return
+	if start_post_user_id:
+		print("Не нашли кто подписался на автора поста")
+		return
 
-    followers_users = bot.get_user_followers(start_post_user_id)
+    for comment_full in bot.get_media_comments_all(media_id, False):
+        comment_author = comment_full["user"]["username"]
+        print(comment_full)
+        if comment_author in prize_candidates or comment_author in bad_users:
+            continue
+        comment = comment_full["text"]
+
+        current_user_and_usercomment = get_mentions(comment)
+        if current_user_and_usercomment:
+            print(comment_author)
+            fritnds = bot.get_user_following(comment_author)
+            for current_user in current_user_and_usercomment:
+                current_user_id_str = str(bot.get_user_id_from_username(current_user))
+                if current_user_id_str in fritnds:
+                    comment_author_id_str = str(comment_full["user_id"])
+                    if comment_author_id_str in liked_users and comment_author_id_str in followers_users:
+                        prize_candidates.add(comment_author)
+                    else:
+                        bad_users.add(comment_author)
+                    break
 
 
-    if not followers_users :
-        print("Список подписчиков Пустой  =  Все остановили ")
-        return
-
-    comment_users = {}
-    all_comments = bot.get_media_comments_all(media_id, True)
-    prize_candidates = set()
-    for current_comment in all_comments:
-        current_user_and_usercomment = get_all_users_from_one_comment(current_comment)
-        for current_user in current_user_and_usercomment[1:] :
-            if is_user_exist(current_user) :
-                comment_users.add(current_user)
-
-    for current_user in comment_users:
-        print("{0}".format(current_user))
-        id_current_user_int = bot.get_user_id_from_username(current_user)
-        id_curtent_user_str = str(id_current_user_int)
-
-        if id_curtent_user_str in followers_users and  id_curtent_user_str in liked_users :
-            prize_candidates.add(current_user)
-
-    if not prize_candidates :
-        print("Список пользователей по всем условиям Пустой !!!")
-        return
-
-    print("Список Пользователей кто выполнил все условия конкурса Instagram")
-    pprint(prize_candidates)
-
+	print("Кандидаты на приз :")
+	print(comment_users)
 
 if __name__ == '__main__':
+    load_dotenv()
+    inst_login = os.getenv("INTGR_LOGIN")
+    inst_passwd = os.getenv("INTGR_PASSWD")
+    bot = Bot()
     main()
